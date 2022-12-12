305c17b4ef6464206b0004e38fd7139b093ef1db
maikel <gmaikel438@gmail.com>
2022-12-12 18:53:27 -0500

    Agregando configuracion en el archivo urls.py de la carpeta de Aplicacion(Catalog)

--- miProyecto/catalog/urls.py
+++ miProyecto/catalog/urls.py
@@ -1,1 +1,13 @@
-urlpatterns = []
+# Agregamos
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    # r'^$' --> es una exprecion irregular,
+    # esto quiere decir cuando la peticion sea barra,
+    # esto significa que empieza y termina por nada,
+    # cuando la peticion sea nada va a tener que disparar
+    # la funcion index del modulo views(views.index)
+    # y se va a llamar index(name='index')
+    url(r'^$', views.index, name='index')
+]
